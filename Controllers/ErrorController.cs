12fba33141158d6930ee54c9a9165a85c98013bc
marvins56 <82571414+marvins56@users.noreply.github.com>
2023-01-29 23:53:33 +0300

    Custom Error page customistion

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMIS.Controllers
+{
+    public class ErrorController : Controller
+    {
+// 400 Status code is call when required page not found type of error
+
+//200 Status code is call when response is ok type of error.
+
+
+//Status Code attribute
+//301 – This code is use to Moved Permanently
+
+//403 – This code is use for Forbidden
+
+//404 – This code is use for Not Found
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+        public ActionResult NotFoundException()
+        {
+            return View();
+        }
+        public ActionResult badrequest()
+        {
+            return View();
+        }
+        public ActionResult internalerror()
+        {
+            return View();
+        }
+    }
+}
